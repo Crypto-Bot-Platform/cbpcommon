1dcfb373b0c166f19ecc54bd544c42dca89f971b
Boris Tsekinovsky <btsekinovsky@breadfinance.com>
2022-02-14 08:31:09 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cbpcommon',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Boris Tsekinovsky',
+    author_email='t.boris@gmail.com',
+    description='Common library for Crypto Bot Platform'
+)
