91199a9d8f131b6f8296665f8aad5aca85b4f75e
Boris Tsekinovsky <btsekinovsky@breadfinance.com>
2022-02-24 06:43:53 -0600

    Added deployment scrip, fixed dependency, updated requirements

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cbpcommon',
-    version='0.0.4',
+    version='0.0.6',
     url='https://github.com/Crypto-Bot-Platform/cbpcommon',
     license='MIT',
     author='Boris Tsekinovsky',
@@ -10,6 +10,6 @@ setup(
     description='Common library for Crypto Bot Platform',
     packages=['.'],
     include_package_data=True,
-    install_requires=["avro==1.11.0", "confluent_kafka==1.8.2", "setuptools==59.6.0", "elasticsearch~=7.16.3",
+    install_requires=["avro==1.11.0", "confluent_kafka==1.8.2", "setuptools>=60.9.0", "elasticsearch~=7.16.3",
                       "jsonpickle"],
 )
